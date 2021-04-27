0251ed770bc4dbd3a9362f10d5f00dbac166117c
kipyatok <kipyatok19@gmail.com>
2021-04-27 20:47:22 +0300

    new

--- src/main/java/web/models/User.java
+++ src/main/java/web/models/User.java
@@ -18,6 +18,25 @@ public class User {
     @Column(name = "email")
     private String email;
 
+    private String userName;
+    private String password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public User() {};
 
     public User(Long id, String name, String lastName, String email) {
